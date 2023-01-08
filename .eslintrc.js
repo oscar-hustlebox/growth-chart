2b84c356187faba5d896875752d3d88155d4c30a
Oscar H <oscar@hustlebox.ca>
2023-01-07 23:04:48 -0700

    feat: add eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,10 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    "linebreak-style": "off",
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+  },
 };
